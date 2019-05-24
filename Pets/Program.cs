5e7e5b0f89fad1038910bc96162789105c2cd0d2
johnsonjoar7@gmail.com <johnsonjoar7@gmail.com>
2019-05-24 17:26:36 +0200

    pet

---
+++ Pets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Program
+    {
+        static List<Pet> pets = new List<Pet>(); //create list
+
+        static void Main(string[] args)
+        {
+
+            pets.Add(new Dog("harry")); //Adds things to the pets list
+            pets.Add(new Lion("simba"));
+            pets.Add(new Duck("sven"));
+
+            Console.WriteLine(pets.Show());
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ Pets/pet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Pet
+    {
+        public string Name { get; set; }
+        public string Sound { get; set; }
+        public bool CanFly { get; set; }
+
+        public string Show()
+        {
+            if (CanFly == true)
+            {
+                return String.Format("name: {0}\nSound: {1}\nType: {2} and can fly", Name, Sound, GetType());
+            }
+            else
+            {
+                return String.Format("name: {0}\nSound: {1}\nType: {2}", Name, Sound, GetType());
+            }
+        }
+
+    }
+    class Dog : Pet
+    {
+        public Dog(String name)
+        {
+            Name = name;
+            Sound = "WOFF!";
+            CanFly = false;
+        }
+    }
+    class Duck : Pet
+    {
+        public Duck(String name)
+        {
+            Name = name;
+            Sound = "Kvack!";
+            CanFly = false;
+        }
+    }
+    class Lion : Pet
+    {
+        public Lion(String name)
+        {
+            Name = name;
+            Sound = "ROAAR!";
+            CanFly = true;
+        }
+    }
+}
